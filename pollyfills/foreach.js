a6a0ce679d44b3f2a775a29f9ee2fc0630b1a56f
Aditya <sharmaaditya13064@gmail.com>
2024-12-20 00:42:45 +0530

    filter polyfill

---
+++ pollyfills/foreach.js
@@ -0,0 +1,7 @@
+// The forEach() executes the callback function on each element of array.
+
+const arr = [1,2,3,4,5,6,7,8];
+
+const value = arr.forEach(item => console.log(item));
+
+console.log('ans',value);
