f3330f96e1c83e22ec56b3488b7ccb521ca77841
Aditya <sharmaaditya13064@gmail.com>
2024-12-20 23:24:07 +0530

    bind polyfill

---
+++ pollyfills/bind.js
@@ -0,0 +1,26 @@
+// The bind() method returns a new function, when invoked, has its this sets to a specific value.
+const objIntro ={
+    name:"aditya",
+    city:"rohtak"
+}
+    
+    function sayIntro (company,place){
+    console.log(`name is ${this.name}, place is ${this.city} and company is ${company} and work place is ${place} `)
+    }
+
+//  let x  =  sayIntro.bind(objIntro,["cognizant","gurgaon"])
+// x();
+
+Function.prototype.newBind = function(context,args){
+    if(typeof this !== "function"){
+        throw new Error(this,"Invalid call");
+    }
+
+    context.fnc = this;
+    return function(...nextArgs){
+        context.fnc(...args,...nextArgs);
+    }
+}
+
+let x = sayIntro.newBind(objIntro,['cast','banglore'])
+x()
