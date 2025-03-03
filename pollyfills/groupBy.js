c1d6af68ff676e0b1c21cc3f5987210c64d2893b
Aditya <a.sharma2@castsoftware.com>
2025-03-03 12:22:28 +0530

    javascript prototype

--- pollyfills/groupBy.js
+++ pollyfills/groupBy.js
@@ -63,3 +63,70 @@ const data = [
 ];
 
 console.log(data.groupBy('category'));
+
+
+//-----------------------------------------------------------------------------------------
+
+// const endorsements = [
+//     { skill: 'css', user: 'Bill' },
+//     { skill: 'javascript', user: 'Chad' },
+//     { skill: 'javascript', user: 'Bill' },
+//     { skill: 'css', user: 'Sue' },
+//     { skill: 'javascript', user: 'Sue' },
+//     { skill: 'html', user: 'Sue' }
+//   ];
+  
+//   const skillsMap = new Map();
+  
+//   // Process endorsements to create skill objects
+//   endorsements.forEach(({ skill, user }) => {
+//     if (!skillsMap.has(skill)) {
+//       skillsMap.set(skill, { skill, users: new Set() });
+//     }
+//     skillsMap.get(skill).users.add(user);
+//   });
+  
+//   // Convert to array and sort by count descending
+//   const skills = Array.from(skillsMap.values()).map(({ skill, users }) => ({
+//     skill,
+//     users: Array.from(users),
+//     count: users.size
+//   })).sort((a, b) => b.count - a.count);
+  
+//   console.log(skills);
+
+const endorsements = [
+    { skill: 'css', user: 'Bill' },
+    { skill: 'javascript', user: 'Chad' },
+    { skill: 'javascript', user: 'Bill' },
+    { skill: 'css', user: 'Sue' },
+    { skill: 'javascript', user: 'Sue' },
+    { skill: 'html', user: 'Sue' }
+  ];
+  
+  const skillsMap = new Map();
+  
+  // Using a for loop instead of forEach
+  for (let i = 0; i < endorsements.length; i++) {
+    const { skill, user } = endorsements[i];
+  
+    if (!skillsMap.has(skill)) {
+      skillsMap.set(skill, { skill, users: new Set() });
+    }
+    skillsMap.get(skill).users.add(user);
+  }
+  
+  // Convert to an array, calculate counts, and sort by count descending
+  const skills = [];
+  for (const [skill, data] of skillsMap) {
+    skills.push({
+      skill,
+      users: Array.from(data.users),
+      count: data.users.size
+    });
+  }
+  
+  skills.sort((a, b) => b.count - a.count);
+  
+  console.log(skills);
+  
